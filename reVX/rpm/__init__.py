3aa5cb4853e1fcbfbaef6e3d869ee0d7dd9cc24e
Michael Rossol <mrossol@gmail.com>
2019-09-11 13:30:48 -0600

    imports update to reVX

--- reVX/rpm/__init__.py
+++ reVX/rpm/__init__.py
@@ -2,5 +2,5 @@
 """
 reV to RPM Pipeline
 """
-from reX.rpm.rpm_manager import RPMClusterManager
-from reX.rpm.rpm_output import RPMOutput
+from reVX.rpm.rpm_manager import RPMClusterManager
+from reVX.rpm.rpm_output import RPMOutput
