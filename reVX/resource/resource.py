94be5772cfd680ee8cf44af6300e49ed85ff1ba4
Michael Rossol <mrossol@gmail.com>
2019-09-24 10:03:52 -0600

    combine files in resource sub-package. Add SolarX, WTKX -> WindX

--- reVX/resource/resource.py
+++ reVX/resource/resource.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Resource Extraction Base
+Resource Extraction Tools
 """
 import gzip
 import logging
@@ -8,7 +8,7 @@ import numpy as np
 import os
 import pandas as pd
 import pickle
-from reV.handlers.resource import Resource
+from reV.handlers.resource import NSRDB, Resource, SolarResource, WindResource
 from scipy.spatial import cKDTree
 
 BIN = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 class ResourceX(Resource):
     """
-    Base class to extract Resource data
+    Resource data extraction tool
     """
     def __init__(self, res_h5, tree=None, **kwargs):
         """
@@ -307,34 +307,6 @@ class ResourceX(Resource):
 
         return region_df
 
-    def get_region_df(self, ds_name, region, region_col='state'):
-        """
-        Extract timeseries of of all sites in given region and return as a
-        DataFrame
-
-        Parameters
-        ----------
-        ds_name : str
-            Dataset to extract
-        region : str
-            Region to search for
-        region_col : str
-            Region column to search
-
-        Return
-        ------
-        region_df : pandas.DataFrame
-            Time-series array of desired dataset for all sites in desired
-            region
-        """
-        gids = self._get_region(region, region_col=region_col)
-        region_df = pd.DataFrame(columns=gids, index=self.time_index)
-        region_df.name = ds_name
-        region_df.index.name = 'time_index'
-        region_df.loc[:, :] = self[ds_name, :, gids]
-
-        return region_df
-
     def get_SAM_df(self, lat_lon):
         """
         Extract time-series of all variables needed to run SAM for nearest
@@ -359,6 +331,103 @@ class ResourceX(Resource):
         SAM_df = []
         for gid in gids:
             SAM_df.append(self['SAM', gid])
+
+        if len(SAM_df) == 1:
+            SAM_df = SAM_df[0]
+
+        return SAM_df
+
+
+class SolarX(SolarResource, ResourceX):
+    """
+    Solar Resource extraction class
+    """
+    def __init__(self, solar_h5, tree=None, **kwargs):
+        """
+        Parameters
+        ----------
+        solar_h5 : str
+            Path to solar .h5 file of interest
+        tree : str
+            path to .pgz file containing pickled cKDTree of lat, lon
+            coordinates
+        kwargs : dict
+            Kwargs for Resource
+        """
+        super().__init__(solar_h5, **kwargs)
+        self._tree = self._init_tree(tree=tree)
+
+
+class NSRDBX(NSRDB, ResourceX):
+    """
+    NSRDB extraction class
+    """
+    def __init__(self, nsrdb_h5, tree=None, **kwargs):
+        """
+        Parameters
+        ----------
+        nsrdb_h5 : str
+            Path to NSRDB .h5 file of interest
+        tree : str
+            path to .pgz file containing pickled cKDTree of lat, lon
+            coordinates
+        kwargs : dict
+            Kwargs for Resource
+        """
+        super().__init__(nsrdb_h5, **kwargs)
+        self._tree = self._init_tree(tree=tree)
+
+
+class WindX(WindResource, ResourceX):
+    """
+    Wind Resource extraction class
+    """
+    def __init__(self, wind_h5, tree=None, **kwargs):
+        """
+        Parameters
+        ----------
+        wind_h5 : str
+            Path to Wind .h5 file of interest
+        tree : str
+            path to .pgz file containing pickled cKDTree of lat, lon
+            coordinates
+        kwargs : dict
+            Kwargs for Resource
+        """
+        super().__init__(wind_h5, **kwargs)
+        self._tree = self._init_tree(tree)
+
+    def get_SAM_df(self, hub_height, lat_lon, **kwargs):
+        """
+        Extract time-series of all variables needed to run SAM for nearest
+        site to given lat_lon and hub height
+
+        Parameters
+        ----------
+        hub_height : int
+            Hub height of interest
+        lat_lon : tuple
+            (lat, lon) coordinate of interest
+        kwargs : dict
+            Internal kwargs for _get_SAM_df:
+            - require_wind_dir
+            - icing
+
+        Return
+        ------
+        SAM_df : pandas.DataFrame | list
+            Time-series DataFrame for given site and dataset
+            If multiple lat, lon pairs are given a list of DatFrames is
+            returned
+        """
+        ds_name = 'SAM_{}m'.format(hub_height)
+        gids = self._get_nearest(lat_lon)
+        if isinstance(gids, int):
+            gids = [gids, ]
+
+        SAM_df = []
+        for gid in gids:
+            SAM_df.append(self._get_SAM_df(ds_name, gid, **kwargs))
 
         if len(SAM_df) == 1:
             SAM_df = SAM_df[0]
