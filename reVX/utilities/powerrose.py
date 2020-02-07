384d4ec95842bab42f30678d2e39ba7c0bcfe95a
Michael Rossol <mrossol@gmail.com>
2020-02-07 14:01:49 -0700

    start powerrose utility

---
+++ reVX/utilities/powerrose.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Aggregate powerrose and sort directions by dominance
+"""
+# from reV.handlers.resource import Resource
+# from reV.supply_curve.aggregation import Aggregate
