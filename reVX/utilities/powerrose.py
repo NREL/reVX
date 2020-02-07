2997794f8a044c9388fe733238efe91bbec5bc15
Michael Rossol <mrossol@gmail.com>
2020-02-07 14:16:08 -0700

    start class

--- reVX/utilities/powerrose.py
+++ reVX/utilities/powerrose.py
@@ -2,5 +2,14 @@
 """
 Aggregate powerrose and sort directions by dominance
 """
-# from reV.handlers.resource import Resource
-# from reV.supply_curve.aggregation import Aggregate
+from reV.supply_curve.aggregation import Aggregation
+
+
+class PowerRoseDirections(Aggregation):
+    """
+    Aggregate PowerRose to Supply Curve points and sort directions in order
+    of prominence using using following key:
+    [[0, 1, 2]
+     [7, x, 3],
+     [6, 5, 4]]
+    """
