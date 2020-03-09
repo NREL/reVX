91ea93b2f95b511f979350f1eecbaff466dcf291
gbuster <grant.buster@nrel.gov>
2020-03-09 12:36:51 -0600

    added auto versioning in new revx outputs handler

---
+++ reVX/handlers/outputs.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Classes to handle reVX h5 output files.
+"""
+
+from reV.handlers.outputs import Outputs as RevOutputs
+from reVX.version import __version__
+
+
+class Outputs(RevOutputs):
+    """
+    Base class to handle reVX output data in .h5 format
+    """
+
+    def set_version_attr(self):
+        """Set the version attribute to the h5 file."""
+        self._h5.attrs['version'] = __version__
