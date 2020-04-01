f9c04c41bad1e11f3990968ebe104bab97edbf39
Michael Rossol <mrossol@gmail.com>
2020-03-31 20:56:31 -0600

    start inheritence

--- reVX/utilities/powerrose.py
+++ reVX/utilities/powerrose.py
@@ -13,3 +13,25 @@ class PowerRoseDirections(Aggregation):
      [7, x, 3],
      [6, 5, 4]]
     """
+
+    def __init__(self, power_rose_h5_fpath, excl_fpath,
+                 agg_dset='powerrose_100m', tm_dset='',
+                 resolution=64):
+        """
+        Parameters
+        ----------
+        excl_fpath : str
+            Filepath to exclusions h5 with techmap dataset.
+        h5_fpath : str
+            Filepath to .h5 file to aggregate
+        tm_dset : str
+            Dataset name in the techmap file containing the
+            exclusions-to-resource mapping data.
+        agg_dset : str
+            Dataset to aggreate, can supply multiple datasets
+        resolution : int | None
+            SC resolution, must be input in combination with gid. Prefered
+            option is to use the row/col slices to define the SC point instead.
+        """
+        super().__init__(excl_fpath, power_rose_h5_fpath, tm_dset, agg_dset,
+                         resolution=resolution)
