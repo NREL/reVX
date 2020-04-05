630d33acb0525ec8bba36b4aed0ef2e22a7073b2
Michael Rossol <mrossol@gmail.com>
2020-04-05 15:04:12 -0600

    finish ProminentWindDirections

--- reVX/utilities/powerrose.py
+++ reVX/utilities/powerrose.py
@@ -10,7 +10,7 @@ from reV.supply_curve.aggregation import Aggregation
 from reV.supply_curve.points import SupplyCurveExtent
 
 
-class SupplyCurvePowerRose(Aggregation):
+class ProminentWindDirections(Aggregation):
     """
     Aggregate PowerRose to Supply Curve points and sort directions in order
     of prominence. Then convert to equivalent sc_point_gid
@@ -39,8 +39,6 @@ class SupplyCurvePowerRose(Aggregation):
         """
         super().__init__(excl_fpath, power_rose_h5_fpath, tm_dset, agg_dset,
                          resolution=resolution)
-
-        self._dir_pos = self._map_direction_pos(power_rose_h5_fpath)
 
     @staticmethod
     def _map_direction_pos(power_rose_h5_fpath):
@@ -130,8 +128,9 @@ class SupplyCurvePowerRose(Aggregation):
         with SupplyCurveExtent(excl_fpath, resolution=resolution) as sc:
             points = sc.points
 
-        neighbor_gids = SupplyCurvePowerRose._compute_neighbors(sc_point_gids,
-                                                                points)
+        neighbor_gids = \
+            ProminentWindDirections._compute_neighbors(sc_point_gids,
+                                                       points)
 
         directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
         neighbor_gids = pd.DataFrame(neighbor_gids,
@@ -140,8 +139,8 @@ class SupplyCurvePowerRose(Aggregation):
 
         return neighbor_gids
 
-    def _get_sc_power_rose(self, excl_area=0.0081, max_workers=None,
-                           chunk_point_len=100):
+    def _get_prominent_directions(self, excl_area=0.0081, max_workers=None,
+                                  chunk_point_len=100):
         """
         Aggregate power rose data to
 
@@ -155,15 +154,93 @@ class SupplyCurvePowerRose(Aggregation):
         chunk_point_len : int, optional
             Number of SC points to process on a single parallel worker,
             by default 100
+
+        Returns
+        -------
+        prominent_dirs : pandas.DataFrame
+            Update meta data table with neighboring supply curve point gids
+            at each cardinal direction and in order of prominent power rose
+            direction
         """
         agg_out = self.aggregate(excl_area=excl_area, max_workers=max_workers,
                                  chunk_point_len=chunk_point_len)
 
         meta = agg_out.pop('meta')
-        powerrose_gids = self._get_neighbors(self._excl_fpath,
-                                             meta['sc_point_gids'].values,
-                                             resolution=self._resolution)
+        neighbor_gids = self._get_neighbors(self._excl_fpath,
+                                            meta['sc_point_gids'].values,
+                                            resolution=self._resolution)
 
-        sc_powerrose = agg_out.pop('powerrose_100m')[self._dir_pos]
-        pr_rank = np.argsort(sc_powerrose.T)
-        pr_rank = np.take_along_axis(powerrose_gids.values, pr_rank, axis=1)
+        dir_pos = self._map_direction_pos(self._h5_fpath)
+        prominent_dirs = agg_out.pop('powerrose_100m')[dir_pos]
+        prominent_dirs = np.argsort(prominent_dirs.T)
+        prominent_dirs = np.take_along_axis(neighbor_gids.values,
+                                            prominent_dirs,
+                                            axis=1)
+
+        columns = ['prominent_direction_{}'.format(i + 1)
+                   for i in range(len(dir_pos))]
+        prominent_dirs = pd.DataFrame(prominent_dirs,
+                                      index=meta['sc_point_gids'].values,
+                                      columns=columns)
+        prominent_dirs = neighbor_gids.join(prominent_dirs)
+        del neighbor_gids
+        prominent_dirs = meta.join(prominent_dirs)
+
+        return prominent_dirs
+
+    @classmethod
+    def run(cls, power_rose_h5_fpath, excl_fpath,
+            agg_dset='powerrose_100m', tm_dset='techmap_wtk',
+            resolution=64, excl_area=0.0081, max_workers=None,
+            chunk_point_len=100, out_fpath=None):
+        """
+        Aggregate powerrose to supply curve points, find neighboring supply
+        curve point gids and rank them based on prominent powerrose direction
+
+        Parameters
+        ----------
+        power_rose_h5_fpath : str
+            Filepath to .h5 file containing powerrose
+        excl_fpath : str
+            Filepath to exclusions h5 with techmap dataset.
+        agg_dset : str, optional
+            Dataset to aggreate, can supply multiple datasets,
+            by default 'powerrose_100m'
+        tm_dset : str, optional
+            Dataset name in the techmap file containing the
+            exclusions-to-resource mapping data,
+            by default 'techmap_wtk'
+        resolution : int, optional
+            SC resolution, must be input in combination with gid. Prefered
+            option is to use the row/col slices to define the SC point instead,
+            by default 64
+        excl_area : float, optional
+            Area of an exclusion cell (square km), by default 0.0081
+        max_workers : int | None, optional
+            Number of cores to run summary on. None is all
+            available cpus, by default None
+        chunk_point_len : int, optional
+            Number of SC points to process on a single parallel worker,
+            by default 100
+        out_fpath : str
+            Path to .csv file to save output table to
+
+        Returns
+        -------
+        prominent_dirs : pandas.DataFrame
+            Update meta data table with neighboring supply curve point gids
+            at each cardinal direction and in order of prominent power rose
+            direction
+        """
+        pr = cls(power_rose_h5_fpath, excl_fpath,
+                 agg_dset=agg_dset, tm_dset=tm_dset,
+                 resolution=resolution)
+
+        prominent_dirs = pr._get_prominent_directions(
+            excl_area=excl_area, max_workers=max_workers,
+            chunk_point_len=chunk_point_len)
+
+        if out_fpath:
+            prominent_dirs.to_csv(out_fpath, index=False)
+
+        return prominent_dirs
