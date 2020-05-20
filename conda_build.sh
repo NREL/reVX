73f3d5e77296d8770b80e5493da1666e8a87197e
Michael Rossol <mrossol@gmail.com>
2020-05-20 12:51:26 -0600

    convert to win but not linux (yet)

--- conda_build.sh
+++ conda_build.sh
@@ -4,32 +4,26 @@ set -e
 
 PKG_NAME=nrel-revx
 
-ARRAY=( 3.7 )
+PY_VERSION=( 3.7 )
 
 export CONDA_BLD_PATH=~/conda-bld
-
-for i in "${ARRAY[@]}"
+platforms=( osx-64 win-64 )
+for py in "${PY_VERSION[@]}"
 do
-	conda-build conda.recipe/ --python $i --channel=nrel
-done
-
-# convert package to other platforms
-platforms=( osx-64 linux-64 win-64 )
-find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
-do
-    echo $file
+	conda build conda.recipe/ --python=$py --channel=nrel --channel=mrossol
+    file=$(conda build conda.recipe/ --python=$py --output)
     for platform in "${platforms[@]}"
     do
-       conda convert --platform $platform $file  -o $CONDA_BLD_PATH/
+       conda convert --platform $platform $file -o $CONDA_BLD_PATH/
     done
 done
 
 # upload packages to conda
 find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
 do
-    echo $file
     anaconda upload -u nrel $file
 done
 
 echo "Building and uploading conda package done!"
 rm -rf $CONDA_BLD_PATH/*
+ls $CONDA_BLD_PATH
