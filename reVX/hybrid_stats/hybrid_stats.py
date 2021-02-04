29d7bb413e2b2363acb41658e32dd95c22933c40
Michael Rossol <mrossol@gmail.com>
2021-02-04 10:19:00 -0700

    add base stats

--- reVX/hybrid_stats/hybrid_stats.py
+++ reVX/hybrid_stats/hybrid_stats.py
@@ -3,7 +3,10 @@
 Module to compute hybrid solar-wind generation statistics
 """
 import logging
+import numpy as np
+import pandas as pd
 from scipy.spatial import cKDTree
+from scipy.stats import pearsonr, spearmanr, kendalltau
 
 from rex.resource import Resource
 from rex.temporal_stats.temporal_stats import TemporalStats
@@ -154,6 +157,190 @@ class HybridStats(TemporalStats):
 
         return time_index, solar_time_slice, wind_time_slice
 
+    @staticmethod
+    def _pearson_correlation(solar_ts, wind_ts):
+        """
+        Compute the Pearson correlation coefficient between the given solar
+        and wind timeseries data. Return just the correlation coefficient.
+
+        Parameters
+        ----------
+        solar_ts : ndarray
+            Solar time-series vector for a single site
+        wind_ts : ndarray
+            Wind time-series vector for a single site
+
+        Returns
+        -------
+        float
+            Pearson correlation coefficient
+        """
+        return pearsonr(solar_ts, wind_ts)[0]
+
+    @staticmethod
+    def _spearman_correlation(solar_ts, wind_ts):
+        """
+        Compute the Spearman correlation coefficient between the given solar
+        and wind timeseries data. Return just the correlation coefficient.
+
+        Parameters
+        ----------
+        solar_ts : ndarray
+            Solar time-series vector for a single site
+        wind_ts : ndarray
+            Wind time-series vector for a single site
+
+        Returns
+        -------
+        float
+            Spearman correlation coefficient
+        """
+        return spearmanr(solar_ts, wind_ts)[0]
+
+    @staticmethod
+    def _kendall_tau(solar_ts, wind_ts):
+        """
+        Compute Kendall's tau correlation between the given solar
+        and wind timeseries data. Return just the correlation coefficient.
+
+        Parameters
+        ----------
+        solar_ts : ndarray
+            Solar time-series vector for a single site
+        wind_ts : ndarray
+            Wind time-series vector for a single site
+
+        Returns
+        -------
+        float
+            Kendall's tau
+        """
+        return kendalltau(solar_ts, wind_ts)[0]
+
+    @classmethod
+    def _compute_stats(cls, res_data, statistics, diurnal=False, month=False):
+        """
+        Compute desired stats for desired time intervals from res_data
+
+        Parameters
+        ----------
+        res_data : pandas.DataFrame
+            DataFrame or resource data. Index is time_index, columns are sites
+        statistics : dict
+            Dictionary of statistic functions/kwargs to run
+        diurnal : bool, optional
+            Extract diurnal stats, by default False
+        month : bool, optional
+            Extract monthly stats, by default False
+
+        Returns
+        -------
+        res_stats : pandas.DataFrame
+            DataFrame of desired statistics at desired time intervals
+        """
+        groupby = []
+        if month:
+            groupby.append(res_data.index.month)
+
+        if diurnal:
+            groupby.append(res_data.index.hour)
+
+        if groupby:
+            res_data = res_data.groupby(groupby)
+
+        res_stats = []
+        for name, stat in statistics.items():
+            func = stat['func']
+            kwargs = stat.get('kwargs', {})
+            s_data = res_data.aggregate(func, **kwargs)
+
+            if groupby:
+                columns = cls._create_names(s_data.index, name)
+                s_data = s_data.T
+                s_data.columns = columns
+            else:
+                s_data = s_data.to_frame(name=name)
+
+            res_stats.append(s_data)
+
+        res_stats = pd.concat(res_stats, axis=1)
+
+        return res_stats
+
+    @classmethod
+    def _extract_stats(cls, res_h5, res_cls, statistics, dataset, hsds=False,
+                       time_index=None, sites_slice=None, diurnal=False,
+                       month=False, combinations=False):
+        """
+        Extract stats for given dataset, sites, and temporal extent
+
+        Parameters
+        ----------
+        res_h5 : str
+            Path to resource h5 file(s)
+        res_cls : Class, optional
+            Resource class to use to access res_h5, by default Resource
+        statistics : dict
+            Dictionary of statistic functions/kwargs to run
+        dataset : str
+            Dataset to extract stats for
+        hsds : bool, optional
+            Boolean flag to use h5pyd to handle .h5 'files' hosted on AWS
+            behind HSDS, by default False
+        time_index : pandas.DatatimeIndex | None, optional
+            Resource DatetimeIndex, if None extract from res_h5,
+            by default None
+        sites_slice : slice | None, optional
+            Sites to extract, if None all, by default None
+        diurnal : bool, optional
+            Extract diurnal stats, by default False
+        month : bool, optional
+            Extract monthly stats, by default False
+        combinations : bool, optional
+            Extract all combinations of temporal stats, by default False
+
+        Returns
+        -------
+        res_stats : pandas.DataFrame
+            DataFrame of desired statistics at desired time intervals
+        """
+        if sites_slice is None:
+            sites_slice = slice(None, None, None)
+
+        with res_cls(res_h5, hsds=hsds) as f:
+            if time_index is None:
+                time_index = f.time_index
+
+            res_data = pd.DataFrame(f[dataset, :, sites_slice],
+                                    index=time_index)
+        if combinations:
+            res_stats = [cls._compute_stats(res_data, statistics)]
+            if month:
+                res_stats.append(cls._compute_stats(res_data, statistics,
+                                                    month=True))
+
+            if diurnal:
+                res_stats.append(cls._compute_stats(res_data, statistics,
+                                                    diurnal=True))
+            if month and diurnal:
+                res_stats.append(cls._compute_stats(res_data, statistics,
+                                                    month=True, diurnal=True))
+
+            res_stats = pd.concat(res_stats, axis=1)
+        else:
+            res_stats = cls._compute_stats(res_data, statistics,
+                                           diurnal=diurnal, month=month)
+
+        if isinstance(sites_slice, slice) and sites_slice.stop:
+            res_stats.index = \
+                list(range(*sites_slice.indices(sites_slice.stop)))
+        elif isinstance(sites_slice, (list, np.ndarray)):
+            res_stats.index = sites_slice
+
+        res_stats.index.name = 'gid'
+
+        return res_stats
+
     def _pre_flight_check(self):
         """
         Compare solar and wind site meta data and time index to ensure
@@ -203,7 +390,9 @@ class HybridStats(TemporalStats):
         stats : dict
             Dictionary of statistic functions/kwargs to run
         """
-        STATS = {'test': self}
+        STATS = {'pearson': self._pearson_correlation,
+                 'spearman': self._spearman_correlation,
+                 'kendall': self._kendal_tau}
         if isinstance(statistics, str):
             statistics = (statistics, )
 
