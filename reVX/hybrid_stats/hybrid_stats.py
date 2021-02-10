55d27b4d70ea76e593c5ba6078b392ef519e7eb8
Michael Rossol <mrossol@gmail.com>
2021-02-09 21:08:17 -0700

    hybrid stats and tests

--- reVX/hybrid_stats/hybrid_stats.py
+++ reVX/hybrid_stats/hybrid_stats.py
@@ -1,36 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Module to compute hybrid solar-wind generation statistics
+Module to compute hybrid solar-wind generation correlations
 """
+from concurrent.futures import as_completed
 import logging
 import numpy as np
+import os
 import pandas as pd
 from scipy.spatial import cKDTree
 from scipy.stats import pearsonr, spearmanr, kendalltau
 
 from rex.resource import Resource
-from rex.temporal_stats.temporal_stats import TemporalStats
+from rex.utilities.execution import SpawnProcessPool
 from rex.utilities.utilities import get_lat_lon_cols
 
 logger = logging.getLogger(__name__)
 
 
-class HybridStats(TemporalStats):
+class HybridStats:
     """
-    Compute comparitive stats for co-located wind and solar generation
+    Compute comparitive correlations for co-located wind and solar generation
     """
     def __init__(self, solar_h5, wind_h5, statistics='pearson',
                  res_cls=Resource, hsds=False):
         """
         Parameters
         ----------
-        res_h5 : str
-            Path to resource h5 file(s)
+        solar_h5 : str
+            Path to solar h5 file(s)
+        wind_h5 : str
+            Path to wind h5 file(s)
         statistics : str | tuple | dict, optional
             Statistics to extract, either a key or tuple of keys in
             cls.STATS, or a dictionary of the form
             {'stat_name': {'func': *, 'kwargs: {**}}},
-            by default 'mean'
+            by default 'pearson'
         res_cls : Class, optional
             Resource class to use to access res_h5, by default Resource
         hsds : bool, optional
@@ -49,15 +53,96 @@ class HybridStats(TemporalStats):
             self._pre_flight_check()
 
     @property
-    def res_h5(self):
+    def solar_h5(self):
         """
-        Path to solar and wind h5 files
+        Path to solar h5 file(s)
 
         Returns
         -------
         str
         """
-        return self._solar_h5, self._wind_h5
+        return self._solar_h5
+
+    @property
+    def wind_h5(self):
+        """
+        Path to wind h5 file(s)
+
+        Returns
+        -------
+        str
+        """
+        return self._wind_h5
+
+    @property
+    def statistics(self):
+        """
+        Dictionary of statistic functions/kwargs to run
+
+        Returns
+        -------
+        dict
+        """
+        return self._stats
+
+    @statistics.setter
+    def statistics(self, statistics):
+        """
+         Statistics to extract, either a key or tuple of keys in
+        cls.STATS, or a dictionary of the form
+        {'stat_name': {'func': *, 'kwargs: {**}}}
+
+        Parameters
+        ----------
+        statistics : dict
+        """
+        self._stats = self._check_stats(statistics)
+
+    @property
+    def res_cls(self):
+        """
+        Resource class to use to access res_h5
+
+        Returns
+        -------
+        Class
+        """
+        return self._res_cls
+
+    @property
+    def time_index(self):
+        """
+        Resource Datetimes
+
+        Returns
+        -------
+        pandas.DatetimeIndex
+        """
+        return self._time_index
+
+    @property
+    def meta(self):
+        """
+        Resource meta-data table
+
+        Returns
+        -------
+        pandas.DataFrame
+        """
+        return self._meta
+
+    @property
+    def lat_lon(self):
+        """
+        Resource (lat, lon) coordinates
+
+        Returns
+        -------
+        pandas.DataFrame
+        """
+        lat_lon_cols = get_lat_lon_cols(self.meta)
+
+        return self.meta[lat_lon_cols]
 
     @staticmethod
     def _map_sites(solar_meta, wind_meta):
@@ -90,16 +175,20 @@ class HybridStats(TemporalStats):
             meta.index.name = 'wind_gid'
             meta = meta.reset_index()
 
-            _, pos = tree.query(meta[wind_cols].values)
-            meta['solar_gid'] = pos
+            dist, pos = tree.query(meta[wind_cols].values)
+            mask = dist <= np.median(dist) * 1.5
+            meta = meta.loc[mask]
+            meta['solar_gid'] = pos[mask]
         else:
             tree = cKDTree(wind_meta[wind_cols].values)
             meta = solar_meta
             meta.index.name = 'solar_gid'
             meta = meta.reset_index()
 
-            _, pos = tree.query(meta[solar_cols])
-            meta['wind_gid'] = pos
+            dist, pos = tree.query(meta[solar_cols])
+            mask = dist <= np.median(dist) * 1.5
+            meta = meta.loc[mask]
+            meta['wind_gid'] = pos[mask]
 
         return meta
 
@@ -313,7 +402,7 @@ class HybridStats(TemporalStats):
             cols = {i: '{}_{}'.format('-'.join(n), s) for i, n
                     in zip(index, column_names)}
             columns_map[s] = cols
-            columns.extend(list(cols.values))
+            columns.extend(list(cols.values()))
 
         return columns_map, columns
 
@@ -358,7 +447,7 @@ class HybridStats(TemporalStats):
             assert solar_grp.shape == wind_grp.shape, msg
             for i in sites:
                 solar_ts = solar_grp.iloc[:, i]
-                wind_ts = solar_grp.ilco[:, i]
+                wind_ts = wind_grp.iloc[:, i]
                 for name, stat in statistics.items():
                     col = cols_map[name][grp_name]
                     func = stat['func']
@@ -376,20 +465,23 @@ class HybridStats(TemporalStats):
 
         Parameters
         ----------
-        res_h5 : str
-            Path to resource h5 file(s)
-        res_cls : Class, optional
-            Resource class to use to access res_h5, by default Resource
+        solar_h5 : str
+            Path to solar h5 file(s)
+        wind_h5 : str
+            Path to wind h5 file(s)
+        sites : pandas.DataFrame
+            Subset of meta DataFrame with sites to extract
         statistics : dict
             Dictionary of statistic functions/kwargs to run
         dataset : str
             Dataset to extract stats for
+        time_index : pandas.DatatimeIndex
+            Timeseries DatetimeIndex
+        res_cls : Class, optional
+            Resource class to use to access res_h5, by default Resource
         hsds : bool, optional
             Boolean flag to use h5pyd to handle .h5 'files' hosted on AWS
             behind HSDS, by default False
-        time_index : pandas.DatatimeIndex | None, optional
-            Resource DatetimeIndex, if None extract from res_h5,
-            by default None
         diurnal : bool, optional
             Extract diurnal stats, by default False
         month : bool, optional
@@ -437,6 +529,28 @@ class HybridStats(TemporalStats):
         out_stats.index.name = 'gid'
 
         return out_stats
+
+    @staticmethod
+    def save_stats(out_stats, out_fpath):
+        """
+        Save correlations to disk
+
+        Parameters
+        ----------
+        out_stats : pandas.DataFrame
+            Table of correlations to save
+        out_path : str
+            Directory, .csv, or .json path to save statistics too
+        """
+        if out_fpath.endswith('.csv'):
+            out_stats.to_csv(out_fpath)
+        elif out_fpath.endswith('.json'):
+            out_stats.to_json(out_fpath)
+        else:
+            msg = ("Cannot save statistics, expecting a directory, .csv, or "
+                   ".json path, but got: {}".format(out_fpath))
+            logger.error(msg)
+            raise OSError(msg)
 
     def _pre_flight_check(self):
         """
@@ -487,9 +601,9 @@ class HybridStats(TemporalStats):
         stats : dict
             Dictionary of statistic functions/kwargs to run
         """
-        STATS = {'pearson': self._pearson_correlation,
-                 'spearman': self._spearman_correlation,
-                 'kendall': self._kendall_tau}
+        STATS = {'pearson': {'func': self._pearson_correlation},
+                 'spearman': {'func': self._spearman_correlation},
+                 'kendall': {'func': self._kendall_tau}}
         if isinstance(statistics, str):
             statistics = (statistics, )
 
@@ -504,3 +618,211 @@ class HybridStats(TemporalStats):
                 assert isinstance(stat['kwargs'], dict), msg
 
         return statistics
+
+    def compute_stats(self, dataset, diurnal=False, month=False,
+                      combinations=False, max_workers=None,
+                      sites_per_worker=1000, lat_lon_only=True):
+        """
+        Compute correlations
+
+        Parameters
+        ----------
+        dataset : str
+            Dataset to extract stats for
+        diurnal : bool, optional
+            Extract diurnal stats, by default False
+        month : bool, optional
+            Extract monthly stats, by default False
+        combinations : bool, optional
+            Extract all combinations of temporal stats, by default False
+        max_workers : None | int, optional
+            Number of workers to use, if 1 run in serial, if None use all
+            available cores, by default None
+        sites_per_worker : int, optional
+            Number of sites to extract on each worker, by default 1000
+        lat_lon_only : bool, optional
+            Only append lat, lon coordinates to stats, by default True
+
+        Returns
+        -------
+        res_stats : pandas.DataFrame
+            DataFrame of desired statistics at desired time intervals
+        """
+        solar_h5 = self.solar_h5
+        wind_h5 = self.wind_h5
+        if max_workers is None:
+            max_workers = os.cpu_count()
+
+        slices = len(self.meta) // sites_per_worker
+        if slices:
+            slices = np.array_split(self.meta, slices)
+        else:
+            slices = [self.meta]
+            max_workers = 1
+
+        if max_workers > 1:
+            msg = ('Extracting {} for {} in parallel using {} workers'
+                   .format(list(self.statistics), dataset, max_workers))
+            logger.info(msg)
+
+            loggers = [__name__, 'rex']
+            with SpawnProcessPool(max_workers=max_workers,
+                                  loggers=loggers) as exe:
+                futures = []
+                for sites in slices:
+                    future = exe.submit(self._extract_stats,
+                                        solar_h5, wind_h5, sites,
+                                        self.statistics, dataset,
+                                        self.time_index, res_cls=self.res_cls,
+                                        hsds=self._hsds, diurnal=diurnal,
+                                        month=month, combinations=combinations)
+                    futures.append(future)
+
+                out_stats = []
+                for i, future in enumerate(as_completed(futures)):
+                    out_stats.append(future.result())
+                    logger.debug('Completed {} out of {} workers'
+                                 .format((i + 1), len(futures)))
+
+            out_stats = pd.concat(out_stats)
+        else:
+            msg = ('Extracting {} for {} in serial'
+                   .format(list(self.statistics), dataset))
+            logger.info(msg)
+            out_stats = []
+            for sites in slices:
+                out_stats.append(self._extract_stats(
+                    solar_h5, wind_h5, sites, self.statistics, dataset,
+                    self.time_index, res_cls=self.res_cls, hsds=self._hsds,
+                    diurnal=diurnal, month=month, combinations=combinations))
+
+            out_stats = pd.concat(out_stats)
+
+        if lat_lon_only:
+            meta = self.lat_lon
+        else:
+            meta = self.meta
+
+        out_stats = meta.join(out_stats.sort_index(), how='inner')
+
+        return out_stats
+
+    @classmethod
+    def run(cls, solar_h5, wind_h5, dataset, statistics='pearson',
+            diurnal=False, month=False, combinations=False,
+            res_cls=Resource, hsds=False, max_workers=None,
+            sites_per_worker=1000, lat_lon_only=True, out_path=None):
+        """
+        Compute temporal stats between solar and wind time-series at desired
+        temporal scales
+
+        Parameters
+        ----------
+        solar_h5 : str
+            Path to solar h5 file(s)
+        wind_h5 : str
+            Path to wind h5 file(s)
+        dataset : str
+            Dataset to extract stats for
+        statistics : str | tuple | dict, optional
+            Statistics to extract, either a key or tuple of keys in
+            cls.STATS, or a dictionary of the form
+            {'stat_name': {'func': *, 'kwargs: {**}}},
+            by default 'pearson'
+        diurnal : bool, optional
+            Extract diurnal stats, by default False
+        month : bool, optional
+            Extract monthly stats, by default False
+        combinations : bool, optional
+            Extract all combinations of temporal stats, by default False
+        res_cls : Class, optional
+            Resource class to use to access res_h5, by default Resource
+        hsds : bool, optional
+            Boolean flag to use h5pyd to handle .h5 'files' hosted on AWS
+            behind HSDS, by default False
+        max_workers : None | int, optional
+            Number of workers to use, if 1 run in serial, if None use all
+            available cores, by default None
+        sites_per_worker : int, optional
+            Number of sites to extract on each worker, by default 1000
+        lat_lon_only : bool, optional
+            Only append lat, lon coordinates to stats, by default True
+        out_path : str, optional
+            Directory, .csv, or .json path to save statistics too,
+            by default None
+
+        Returns
+        -------
+        out_stats : pandas.DataFrame
+            DataFrame of resource statistics
+        """
+        hybrid_stats = cls(solar_h5, wind_h5, statistics=statistics,
+                           res_cls=res_cls, hsds=hsds)
+        out_stats = hybrid_stats.compute_stats(
+            dataset, diurnal=diurnal, month=month, combinations=combinations,
+            max_workers=max_workers, sites_per_worker=sites_per_worker,
+            lat_lon_only=lat_lon_only)
+        if out_path is not None:
+            hybrid_stats.save_stats(out_stats, out_path)
+
+        return out_stats
+
+    @classmethod
+    def cf_profile(cls, solar_h5, wind_h5, statistics='pearson',
+                   diurnal=False, month=False, combinations=False,
+                   res_cls=Resource, hsds=False, max_workers=None,
+                   sites_per_worker=1000, lat_lon_only=True, out_path=None):
+        """
+        Compute temporal stats on cf_profile dataset
+
+        Parameters
+        ----------
+        solar_h5 : str
+            Path to solar h5 file(s)
+        wind_h5 : str
+            Path to wind h5 file(s)
+        dataset : str
+            Dataset to extract stats for
+        statistics : str | tuple | dict, optional
+            Statistics to extract, either a key or tuple of keys in
+            cls.STATS, or a dictionary of the form
+            {'stat_name': {'func': *, 'kwargs: {**}}},
+            by default 'pearson'
+        diurnal : bool, optional
+            Extract diurnal stats, by default False
+        month : bool, optional
+            Extract monthly stats, by default False
+        combinations : bool, optional
+            Extract all combinations of temporal stats, by default False
+        res_cls : Class, optional
+            Resource class to use to access res_h5, by default Resource
+        hsds : bool, optional
+            Boolean flag to use h5pyd to handle .h5 'files' hosted on AWS
+            behind HSDS, by default False
+        max_workers : None | int, optional
+            Number of workers to use, if 1 run in serial, if None use all
+            available cores, by default None
+        sites_per_worker : int, optional
+            Number of sites to extract on each worker, by default 1000
+        lat_lon_only : bool, optional
+            Only append lat, lon coordinates to stats, by default True
+        out_path : str, optional
+            Directory, .csv, or .json path to save statistics too,
+            by default None
+
+        Returns
+        -------
+        out_stats : pandas.DataFrame
+            DataFrame of resource statistics
+        """
+        hybrid_stats = cls(solar_h5, wind_h5, statistics=statistics,
+                           res_cls=res_cls, hsds=hsds)
+        out_stats = hybrid_stats.compute_stats(
+            'cf_profile', diurnal=diurnal, month=month,
+            combinations=combinations, max_workers=max_workers,
+            sites_per_worker=sites_per_worker,
+            lat_lon_only=lat_lon_only)
+        if out_path is not None:
+            hybrid_stats.save_stats(out_stats, out_path)
+
+        return out_stats
---
+++ tests/test_hybrid_stats.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+pytests for  Rechunk h5
+"""
+import numpy as np
+import os
+import pytest
+from scipy.stats import pearsonr, spearmanr, kendalltau
+
+from rex.resource import Resource
+from reVX import TESTDATADIR
+from reVX.hybrid_stats.hybrid_stats import HybridStats
+
+FUNCS = {'pearson': pearsonr, 'spearman': spearmanr, 'kendall': kendalltau}
+SOLAR_H5 = os.path.join(TESTDATADIR, 'hybrid_stats', 'hybrid_solar_2012.h5')
+WIND_H5 = os.path.join(TESTDATADIR, 'hybrid_stats', 'hybrid_wind_2012.h5')
+DATASET = 'cf_profile'
+
+META = HybridStats(SOLAR_H5, WIND_H5).meta
+with Resource(SOLAR_H5) as f:
+    SOLAR = f[DATASET, :, META['solar_gid'].values]
+
+with Resource(WIND_H5) as f:
+    WIND = f[DATASET, :, META['wind_gid'].values]
+    TIME_INDEX = f.time_index
+
+
+def compute_stats(func, solar, wind):
+    """
+    Compute pair-wise stats between solar and wind profiles
+    """
+    stats = []
+    for s, w in zip(solar.T, wind.T):
+        stats.append(func(s, w)[0])
+
+    return np.array(stats, dtype=np.float32)
+
+
+@pytest.mark.parametrize(("max_workers", "func"),
+                         [(1, 'pearson'),
+                          (None, 'pearson'),
+                          (None, 'spearman'),
+                          (None, 'kendall')])
+def test_hybrid_stats(max_workers, func):
+    """
+    Test HybridStats Pearsons Correlation
+    """
+    test_stats = HybridStats.cf_profile(SOLAR_H5, WIND_H5,
+                                        statistics=func,
+                                        month=True,
+                                        diurnal=True,
+                                        combinations=True,
+                                        max_workers=max_workers)
+
+    gids = META.index.values
+    msg = ('gids do not match!')
+    assert np.allclose(gids, test_stats.index.values), msg
+
+    function = FUNCS[func]
+
+    truth = compute_stats(function, SOLAR, WIND)
+    test = test_stats[f'2012_{func}'].values
+    msg = 'Correlation coefficients do not match!'
+    assert np.allclose(truth, test, equal_nan=True), msg
+
+    mask = TIME_INDEX.month == 1
+    truth = compute_stats(function, SOLAR[mask], WIND[mask])
+    test = test_stats[f'2012-Jan_{func}'].values
+    msg = 'January correlations do not match!'
+    assert np.allclose(truth, test, equal_nan=True), msg
+
+    mask = TIME_INDEX.hour == 18
+    truth = compute_stats(function, SOLAR[mask], WIND[mask])
+    test = test_stats[f'2012-18_{func}'].values
+    msg = '18:00 correlations do not match!'
+    assert np.allclose(truth, test, equal_nan=True), msg
+
+    mask = (TIME_INDEX.month == 7) & (TIME_INDEX.hour == 18)
+    truth = compute_stats(function, SOLAR[mask], WIND[mask])
+    test = test_stats[f'2012-July-18_{func}'].values
+    msg = 'July-18:00 correlations do not match!'
+    assert np.allclose(truth, test, equal_nan=True), msg
+
+
+def execute_pytest(capture='all', flags='-rapP'):
+    """Execute module as pytest with detailed summary report.
+
+    Parameters
+    ----------
+    capture : str
+        Log or stdout/stderr capture option. ex: log (only logger),
+        all (includes stdout/stderr)
+    flags : str
+        Which tests to show logs and results for.
+    """
+
+    fname = os.path.basename(__file__)
+    pytest.main(['-q', '--show-capture={}'.format(capture), fname, flags])
+
+
+if __name__ == '__main__':
+    execute_pytest()
