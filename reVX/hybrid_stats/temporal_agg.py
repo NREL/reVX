ea6ec1fc3793f44587b5e9634ec801850ccd69e2
Michael Rossol <mrossol@gmail.com>
2021-05-25 15:05:46 -0600

    start temporal agg

---
+++ reVX/hybrid_stats/temporal_agg.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Module to temporaly aggregate solar and wind CF profiles
+"""
+import logging
+import os
+
+from reVX.handlers.outputs import Outputs
+
+
+class TemporalAgg():
+    """
+    Class to temporally aggregate time-series data
+    """
+    def __init__(self, h5_fpath, freq='doy'):
+        pass
