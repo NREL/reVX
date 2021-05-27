5d780166377a7d417a6693a354aae180d0051808
mbannist <mike.bannister@nrel.gov>
2021-05-27 11:30:35 -0600

    Minor change to reprojection script

--- reVX/x_mission_paths/rasterization/proj_shp.sh
+++ reVX/x_mission_paths/rasterization/proj_shp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Reproject iso regions to CONUS exclusion template
+# $1 is out file
+# $2 is input file
 
 ogr2ogr \
 -t_srs '+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs' \
-iso_regions_template_crs.shp \
-ISO_Regions.shp 
+$1 $2
 
