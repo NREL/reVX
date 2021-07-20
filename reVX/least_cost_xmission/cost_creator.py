f5f4a2227e65e89986e53e8275b4effbebeede42
Michael Rossol <mrossol@gmail.com>
2021-07-20 13:33:33 -0600

    add notes to XmissionCostCreator fix docstring in excl converter

--- reVX/least_cost_xmission/cost_creator.py
+++ reVX/least_cost_xmission/cost_creator.py
@@ -12,4 +12,8 @@ logger = logging.getLogger(__name__)
 class XmissionCostCreator(ExclusionsConverter):
     """
     Class to create and save Transmission cost layers to a .h5 Exclusion file
+    - dist_to_coast (for valid sc_points)
+    - base_costs
+    - multiplier_*mw
+    - xmission_barrier
     """
