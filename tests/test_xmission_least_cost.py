c239740b6969e5a6d5eaf76c3904ea9fb05f6506
mbannist <mike.bannister@nrel.gov>
2022-06-08 10:14:10 -0600

    Fix test broken by aoswt code

--- tests/test_xmission_least_cost.py
+++ tests/test_xmission_least_cost.py
@@ -71,7 +71,7 @@ def runner():
 
 
 @pytest.mark.parametrize('capacity', [100, 200, 400, 1000])
-def _test_capacity_class(capacity):
+def test_capacity_class(capacity):
     """
     Test least cost xmission and compare with baseline data
     """
@@ -98,7 +98,7 @@ def _test_capacity_class(capacity):
 
 
 @pytest.mark.parametrize('max_workers', [1, None])
-def _test_parallel(max_workers):
+def test_parallel(max_workers):
     """
     Test least cost xmission and compare with baseline data
     """
@@ -127,7 +127,7 @@ def _test_parallel(max_workers):
 
 
 @pytest.mark.parametrize('resolution', [64, 128])
-def _test_resolution(resolution):
+def test_resolution(resolution):
     """
     Test least cost xmission and compare with baseline data
     """
@@ -193,8 +193,7 @@ def test_cli(runner):
                .format(traceback.print_exception(*result.exc_info)))
         assert result.exit_code == 0, msg
 
-        test = '{}_{}MW_128.csv'.format(os.path.basename(td),
-                                                          capacity)
+        test = '{}_{}MW_128.csv'.format(os.path.basename(td), capacity)
         test = os.path.join(td, test)
         test = pd.read_csv(test)
         SupplyCurve._check_substation_conns(test, sc_cols='sc_point_gid')
