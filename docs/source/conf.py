9acafd3690f11eb58e5bc0444c71cd819e6000c2
ppinchuk <ppinchuk@nrel.gov>
2023-04-14 17:58:04 -0600

    Add myst_parser dependency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,8 +57,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
     'sphinx_click.ext',
-    "sphinx_tabs.tabs",
-    "sphinx_copybutton",
+    "myst_parser",
 ]
 
 intersphinx_mapping = {
