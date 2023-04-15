9f0669f59d80547f96cef366b83b75083bc1af6b
ppinchuk <ppinchuk@nrel.gov>
2023-04-14 18:00:57 -0600

    Add sphinx copybutton and tabs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,8 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
     'sphinx_click.ext',
+    "sphinx_tabs.tabs",
+    "sphinx_copybutton",
     "myst_parser",
 ]
 
@@ -82,7 +84,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
