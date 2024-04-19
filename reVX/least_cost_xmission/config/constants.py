c2350a46afd46f340841be12156a669876b3d7ba
Paul Pinchuk <ppinchuk@nrel.gov>
2024-04-19 15:47:11 -0600

    Add landfall tiff as const

--- reVX/least_cost_xmission/config/constants.py
+++ reVX/least_cost_xmission/config/constants.py
@@ -7,6 +7,7 @@ DRY_COSTS_H5_LAYER_NAME = 'dry_costs'
 BARRIER_H5_LAYER_NAME = 'transmission_barrier'  # Combined friction and barrier
 
 WET_COSTS_TIFF = 'wet_costs.tif'
+LANDFALL_COSTS_TIFF = 'landfall_costs.tif'
 FRICTION_TIFF = 'friction.tif'
 RAW_BARRIER_TIFF = 'barriers.tif'
 BARRIER_TIFF = 'combined_barriers.tif'  # File for combo friction and barriers
