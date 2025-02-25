b6b343653f3477c9cd48b27993992c9436afb89a
Paul Pinchuk <ppinchuk@nrel.gov>
2025-02-25 15:05:55 -0700

    Add min value of barrier

--- reVX/least_cost_xmission/trans_cap_costs.py
+++ reVX/least_cost_xmission/trans_cap_costs.py
@@ -390,7 +390,7 @@ class TieLineCosts:
 
         # TODO: Add user option of specifying wether 0 and neg should
         # be hard barriers.
-        max_val = np.max(self._mcp_cost)
+        max_val = max(1e15, np.max(self._mcp_cost))
         self._mcp_cost = np.where(self._mcp_cost <= 0, max_val, self._mcp_cost)
         logger.debug("MCP cost min: %.2f, max: %.2f, median: %.2f",
                      np.min(self._mcp_cost), np.max(self._mcp_cost),
