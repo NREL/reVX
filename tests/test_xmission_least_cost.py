b72ae24e774bcbe86453da6304f56c947a48e5a5
ppinchuk <pinchukpaul@gmail.com>
2025-03-19 15:53:15 -0600

    Remove test for now

--- tests/test_xmission_least_cost.py
+++ tests/test_xmission_least_cost.py
@@ -483,104 +483,104 @@ def test_tracked_layers():
 
 #     LOGGERS.clear()
 
+# TODO: Uncomment when user can specify hard barriers again
+# def test_regional_cli_new_layer_names(runner, ri_ba):
+#     """
+#     Test Regional cost routines and CLI
+#     """
+#     ri_feats = gpd.clip(gpd.read_file(FEATURES), ri_ba.buffer(10_000))
 
-def test_regional_cli_new_layer_names(runner, ri_ba):
-    """
-    Test Regional cost routines and CLI
-    """
-    ri_feats = gpd.clip(gpd.read_file(FEATURES), ri_ba.buffer(10_000))
+#     with tempfile.TemporaryDirectory() as td:
+#         ri_feats_path = os.path.join(td, 'ri_feats.gpkg')
+#         ri_feats.to_file(ri_feats_path, driver="GPKG", index=False)
 
-    with tempfile.TemporaryDirectory() as td:
-        ri_feats_path = os.path.join(td, 'ri_feats.gpkg')
-        ri_feats.to_file(ri_feats_path, driver="GPKG", index=False)
+#         ri_ba_path = os.path.join(td, 'ri_ba.gpkg')
+#         ri_ba.to_file(ri_ba_path, driver="GPKG", index=False)
 
-        ri_ba_path = os.path.join(td, 'ri_ba.gpkg')
-        ri_ba.to_file(ri_ba_path, driver="GPKG", index=False)
+#         ri_substations_path = os.path.join(td, 'ri_subs.gpkg')
+#         result = runner.invoke(lcp_main,
+#                                ['map-ss-to-rr',
+#                                 '-feats', ri_feats_path,
+#                                 '-regs', ri_ba_path,
+#                                 '-rid', "ba_str",
+#                                 '-of', ri_substations_path])
+#         msg = ('Failed with error {}'
+#                .format(traceback.print_exception(*result.exc_info)))
+#         assert result.exit_code == 0, msg
 
-        ri_substations_path = os.path.join(td, 'ri_subs.gpkg')
-        result = runner.invoke(lcp_main,
-                               ['map-ss-to-rr',
-                                '-feats', ri_feats_path,
-                                '-regs', ri_ba_path,
-                                '-rid', "ba_str",
-                                '-of', ri_substations_path])
-        msg = ('Failed with error {}'
-               .format(traceback.print_exception(*result.exc_info)))
-        assert result.exit_code == 0, msg
+#         cost_h5_path = os.path.join(td, 'costs.h5')
+#         shutil.copy(COST_H5, cost_h5_path)
 
-        cost_h5_path = os.path.join(td, 'costs.h5')
-        shutil.copy(COST_H5, cost_h5_path)
+#         with h5py.File(cost_h5_path, "a") as fh:
+#             tb = fh["transmission_barrier"][:]
+#             iso = fh["ISO_regions"][:]
 
-        with h5py.File(cost_h5_path, "a") as fh:
-            tb = fh["transmission_barrier"][:]
-            iso = fh["ISO_regions"][:]
+#             del fh["transmission_barrier"]
+#             del fh["ISO_regions"]
+#             assert "transmission_barrier" not in fh.keys()
+#             assert "ISO_regions" not in fh.keys()
 
-            del fh["transmission_barrier"]
-            del fh["ISO_regions"]
-            assert "transmission_barrier" not in fh.keys()
-            assert "ISO_regions" not in fh.keys()
+#             fh.create_dataset("tb", data=tb)
+#             fh.create_dataset("iso", data=iso)
 
-            fh.create_dataset("tb", data=tb)
-            fh.create_dataset("iso", data=iso)
+#         config = {
+#             "log_directory": td,
+#             "execution_control": {
+#                 "option": "local",
+#             },
+#             "cost_fpath": cost_h5_path,
+#             "features_fpath": ri_substations_path,
+#             "regions_fpath": ri_ba_path,
+#             "region_identifier_column": "ba_str",
+#             "capacity_class": 1000,
+#             "cost_layers": [{"layer_name": "tie_line_costs_1500MW"}],
+#             "friction_layers": [{"layer_name": LCP_AGG_COST_LAYER_NAME,
+#                                  "multiplier_layer": "tb",
+#                                  "multiplier_scalar": 100}],
+#             "min_line_length": 0,
+#             "save_paths": False,
+#             "iso_regions_layer_name": "iso",
+#         }
 
-        config = {
-            "log_directory": td,
-            "execution_control": {
-                "option": "local",
-            },
-            "cost_fpath": cost_h5_path,
-            "features_fpath": ri_substations_path,
-            "regions_fpath": ri_ba_path,
-            "region_identifier_column": "ba_str",
-            "capacity_class": 1000,
-            "cost_layers": [{"layer_name": "tie_line_costs_1500MW"}],
-            "friction_layers": [{"layer_name": LCP_AGG_COST_LAYER_NAME,
-                                 "multiplier_layer": "tb",
-                                 "multiplier_scalar": 100}],
-            "min_line_length": 0,
-            "save_paths": False,
-            "iso_regions_layer_name": "iso",
-        }
+#         config_path = os.path.join(td, 'config.json')
+#         with open(config_path, 'w') as f:
+#             json.dump(config, f)
 
-        config_path = os.path.join(td, 'config.json')
-        with open(config_path, 'w') as f:
-            json.dump(config, f)
+#         result = runner.invoke(main, ['from-config',
+#                                       '-c', config_path, '-v'])
+#         msg = ('Failed with error {}'
+#                .format(traceback.print_exception(*result.exc_info)))
+#         assert result.exit_code == 0, msg
 
-        result = runner.invoke(main, ['from-config',
-                                      '-c', config_path, '-v'])
-        msg = ('Failed with error {}'
-               .format(traceback.print_exception(*result.exc_info)))
-        assert result.exit_code == 0, msg
+#         test = '{}_1000_128.csv'.format(os.path.basename(td))
+#         test = os.path.join(td, test)
+#         test = pd.read_csv(test)
 
-        test = '{}_1000_128.csv'.format(os.path.basename(td))
-        test = os.path.join(td, test)
-        test = pd.read_csv(test)
+#         assert "poi_lat" in test
+#         assert "poi_lon" in test
+#         assert "ba_str" in test
 
-        assert "poi_lat" in test
-        assert "poi_lon" in test
-        assert "ba_str" in test
+#         assert len(test) == 1036
+#         # trans_gid has 1 duplicate substation entry
+#         assert len(set(test.trans_gid.unique())) == 69
+#         assert len(set(test.poi_gid.unique())) == 68
+#         assert set(test.ba_str.unique()) == {"p1", "p2", "p3", "p4"}
 
-        assert len(test) == 1036
-        # trans_gid has 1 duplicate substation entry
-        assert len(set(test.trans_gid.unique())) == 69
-        assert len(set(test.poi_gid.unique())) == 68
-        assert set(test.ba_str.unique()) == {"p1", "p2", "p3", "p4"}
+#         mask = test['max_volts'] >= 500
 
-        mask = test['max_volts'] >= 500
+#         assert len(test[mask]) == 13
+#         assert set(test[mask].trans_gid.unique()) == {69130}
+#         assert set(test[mask].ba_str.unique()) == {"p4"}
 
-        assert len(test[mask]) == 13
-        assert set(test[mask].trans_gid.unique()) == {69130}
-        assert set(test[mask].ba_str.unique()) == {"p4"}
+#         assert len(test[mask].poi_lat.unique()) == 1
+#         assert len(test[mask].poi_lon.unique()) == 1
 
-        assert len(test[mask].poi_lat.unique()) == 1
-        assert len(test[mask].poi_lon.unique()) == 1
+#         assert np.allclose(test["tie_line_costs_1500MW_cost"].astype(float),
+#                            test["raw_line_cost"].astype(float))
+#         assert np.allclose(test["tie_line_costs_1500MW_dist_km"].astype(float),
+#                            test["dist_km"].astype(float))
 
-        assert np.allclose(test["tie_line_costs_1500MW_cost"].astype(float),
-                           test["raw_line_cost"].astype(float))
-        assert np.allclose(test["tie_line_costs_1500MW_dist_km"].astype(float),
-                           test["dist_km"].astype(float))
-
-    LOGGERS.clear()
+#     LOGGERS.clear()
 
 
 def execute_pytest(capture='all', flags='-rapP'):
